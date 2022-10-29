3f5045f4b715bbcca13bac9c3df086bbaf7cf7ec
dark-tulip <89765480+dark-tulip@users.noreply.github.com>
2022-10-29 17:14:23 +0600

    Update CreateUsers.ps1

--- CreateUsers.ps1
+++ CreateUsers.ps1
@@ -1,1 +1,17 @@
-New-ADUser -Name Tansh -AccountExpirationDate 12.12.2023 -AccountPassword 12345Zxc -AllowReversiblePasswordEncryption trur -City Almaty -Company MyCompany -Initials TTUser -Office LikeHR -OfficePhone 87776665544 -PasswordNeverExpires true -StreetAddress almatyStreet -Surname telbayeva -Title myTitle -Type user -UserPrincipalName tanshusername
+New-ADUser -Name Tansh 
+-AccountExpirationDate 12.12.2023 
+-AccountPassword 12345Zxc 
+-AllowReversiblePasswordEncryption true 
+-City Almaty 
+-Company MyCompany 
+-Initials TTUser 
+-Office LikeHR 
+-OfficePhone 87776665544 
+-PasswordNeverExpires true 
+-StreetAddress almatyStreet 
+-Surname telbayeva 
+-Title myTitle
+-Type user
+-UserPrincipalName tanshusername
+
+<# This is a comment #>
