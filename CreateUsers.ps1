9c4f9781446c0520f2e42f043551baca2b812c85
dark-tulip <89765480+dark-tulip@users.noreply.github.com>
2022-10-29 17:30:11 +0600

    Update CreateUsers.ps1

--- CreateUsers.ps1
+++ CreateUsers.ps1
@@ -1,17 +1,8 @@
-New-ADUser -Name Tansh 
--AccountExpirationDate 12.12.2023 
--AccountPassword 12345Zxc 
--AllowReversiblePasswordEncryption true 
--City Almaty 
--Company MyCompany 
--Initials TTUser 
--Office LikeHR 
--OfficePhone 87776665544 
--PasswordNeverExpires true 
--StreetAddress almatyStreet 
--Surname telbayeva 
--Title myTitle
--Type user
--UserPrincipalName tanshusername
-
+New-ADUser `
+ -SamAccountName TanshTelb3 -Name TanshTelb3 -GivenName Tansh3 `
+ -Surname Telb -DisplayName 'Tansh Telb3' -Path 'CN=Users,DC=tansh,DC=kz' `
+ -CannotChangePassword $true  -ChangePasswordAtLogon $false -City 'Almaty'  `
+ -State 'Almaty’ -Country RK -Department ‘Accounting’ -Title ’Saler' `
+ -EmailAddress 'telbayeva@tansh.kz' -Enabled $true -AccountPassword (Read-Host -AsSecureString "12345Zxc") 
+ 
 <# This is a comment #>
