17912e3cd3fb672d9d4b4a252dc14fbad96d97c5
dark-tulip <89765480+dark-tulip@users.noreply.github.com>
2022-10-29 18:52:09 +0600

    Update CreateUsers.ps1

--- CreateUsers.ps1
+++ CreateUsers.ps1
@@ -6,3 +6,24 @@ New-ADUser `
  -EmailAddress 'telbayeva@tansh.kz' -Enabled $true -AccountPassword (Read-Host -AsSecureString "12345Zxc") 
  
 <# This is a comment #>
+
+New-ADUser `
+ -SamAccountName TanshTelb3 -Name TanshTelb333 -GivenName Tansh3 `
+ -Surname Telb -DisplayName 'Tansh Telb3' -Path 'CN=Users,DC=tansh,DC=kz' `
+ -CannotChangePassword $true  -ChangePasswordAtLogon $false -City 'Almaty'  `
+ -State 'Almaty’ -Country RK -Department ‘Accounting’ -Title ’Saler' `
+ -EmailAddress 'telbayeva@tansh.kz' -Enabled $true -AccountPassword (Read-Host -AsSecureString "12345Zxc")
+
+ 
+
+Remove-ADUser -Identity TanshTelb3
+
+ 
+ 
+ Set-ADAccountPassword -Identity 'CN=Elisa Daugherty,OU=Accounts,DC=Fabrikam,DC=com' `
+-Reset -NewPassword (ConvertTo-SecureString -AsPlainText "p@ssw0rd" -Force)
+
+ 
+<#
+-AccountPassword (Read-Host -AsSecureString "12345Zxc")     
+ New-ADUser -Name Tansh -AccountExpirationDate 12.12.2023 -AccountPassword 12345Zxc -AllowReversiblePasswordEncryption trur -City Almaty -Company MyCompany -Initials TTUser -Office LikeHR -OfficePhone 87776665544 -PasswordNeverExpires true -StreetAddress almatyStreet -Surname telbayeva -Title myTitle -Type user -UserPrincipalName tanshusername #>
